cf56e59c30905ed3d718bd3588e8d8c92f5a4311
tomgrim1 <tgrimley1@gmail.com>
2013-02-20 21:21:06 +0000

    Following other users is now working

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,7 @@
 class Relationship < ActiveRecord::Base
   attr_accessible :followed_id, :follower_id
 
-  belongs_to :follower, classname: 'User'
-  belongs_to :followed, classname: 'User'
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :ribbits
 
-  has_many :follower_relationships, classname: 'Relationship', foreign_key: 'followed_id'
-  has_many :followed_relationships, classname: 'Relationship', foreign_key: 'follower_id'
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'followed_id'
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
 
   has_many :followers, through: :follower_relationships
   has_many :followeds, through: :followed_relationships
