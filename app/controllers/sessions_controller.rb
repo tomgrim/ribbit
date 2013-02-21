ed73417a8988e76a279985a0890695e0ebcd2f3a
tomgrim1 <tgrimley1@gmail.com>
2013-02-21 20:43:00 +0000

    Fixed bug signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_username(params[:useranme])
-  	if user && user.authenciate(params[:password])
+  	user = User.find_by_username(params[:username])
+  	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to rooturl, notice: 'Logged in!'
+  		redirect_to root_url, notice: 'Logged in!'
   	else
   		flash[:error] = 'Wrong Username or Password.'
   		redirect_to root_url
